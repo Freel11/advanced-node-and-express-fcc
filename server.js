72aab607af285e3dd98132df75d45b18148dd2e9
jade@itnt.co.za <jade@itnt.co.za>
2022-07-26 22:55:17 +0200

    implementing an authentication strategy

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const passport = require('passport');
 const session = require('express-session');
 const ObjectID = require('mongodb').ObjectID
+const LocalStrategy = require('passport-local')
 
 const app = express();
 
@@ -28,6 +29,18 @@ app.set('view engine', 'pug')
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users')
+
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log(`User ${username} attempted to log in.`)
+        if (err) { return done(err) }
+        if (!user) { return done(null, false) }
+        if (password !== user.password) { return done(null, false) }
+        return done(null, user)
+      })
+    }
+  ))
 
   app.route('/').get((req, res) => {
     res.render('pug', {title: 'Connected to Database', message: 'Please login'});
