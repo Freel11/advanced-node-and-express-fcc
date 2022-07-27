3d60070ac1232a9c6138513376fcdda1f6aaf14a
jade@itnt.co.za <jade@itnt.co.za>
2022-07-27 13:00:52 +0200

    adding a logout route and a 404 page

--- server.js
+++ server.js
@@ -50,10 +50,21 @@ myDB(async client => {
     res.render('/profile', { username: req.user.username })
   })
 
+  app.route('/logout').get(ensureAuthenticated, (req, res) => {
+    req.logout()
+    res.redirect('/')
+  })
+
   app.post('/login',
     passport.authenticate('local', { failureRedirect: '/' }),
     (req, res) => {
       res.redirect('/profile')
+  })
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found')
   })
 
   passport.serializeUser((user, done) => {
