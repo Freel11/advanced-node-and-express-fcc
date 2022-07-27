2dbd71a9806e0ee108ca00c58aad9fbedb19ed5f
jade@itnt.co.za <jade@itnt.co.za>
2022-07-27 22:18:12 +0200

    completing the github strategy function

--- auth.js
+++ auth.js
@@ -19,12 +19,34 @@ module.exports = function (app, myDataBase) {
   ))
 
   passport.use(new GithubStrategy({
-  	clientID: process.env.GITHUB_CLIENT_ID,
-  	clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  	callbackURL: 'https://advanced-node-and-express-fcc.herokuapp.com/auth/github/callback'
-  },
-  function(accessToken, refreshToken, profile, cb) {
-  	console.log(profile)
+	  	clientID: process.env.GITHUB_CLIENT_ID,
+	  	clientSecret: process.env.GITHUB_CLIENT_SECRET,
+	  	callbackURL: 'https://advanced-node-and-express-fcc.herokuapp.com/auth/github/callback'
+	},
+    function(accessToken, refreshToken, profile, cb) {
+  	 myDataBase.findOneAndUpdate(
+      { id: profile.id },
+      {
+        $setOnInsert: {
+          id: profile.id,
+          name: profile.displayName || 'John Doe',
+          photo: profile.photos[0].value || '',
+          email: Array.isArray(profile.emails) ? profile.emails[0].value : 'No public email',
+          created_on: new Date(),
+          provider: profile.provider || ''
+        },
+        $set: {
+          last_login: new Date()
+        },
+        $inc: {
+          login_count: 1
+        }
+      },
+      { upsert: true, new: true },
+      (err, doc) => {
+        return cb(null, doc.value)
+      }
+      )
   }))
 
   passport.serializeUser((user, done) => {
