860113842d164f92b36aaee168ba59edb1d1f429
jade@itnt.co.za <jade@itnt.co.za>
2022-07-27 14:15:52 +0200

    installed bcrypt as a dependency and implemented password hashing

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const session = require('express-session');
 const ObjectID = require('mongodb').ObjectID
 const LocalStrategy = require('passport-local')
+const bcrypt = require('bcrypt')
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use(passport.session())
 
 app.set('view engine', 'pug')
 
+
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users')
 
@@ -36,7 +38,7 @@ myDB(async client => {
         console.log(`User ${username} attempted to log in.`)
         if (err) { return done(err) }
         if (!user) { return done(null, false) }
-        if (password !== user.password) { return done(null, false) }
+        if (!bcrypt.compareSync(password, user.password)) { return done(null, false) }
         return done(null, user)
       })
     }
@@ -64,6 +66,7 @@ myDB(async client => {
   })
 
   app.route('/register').post((req, res, next) => {
+    const hash = bcrypt.hashSync(req.body.password, 12)
     myDataBase.findOne({ username: req.body.username }, (err, user) => {
       if (err) {
         next(err)
@@ -72,7 +75,7 @@ myDB(async client => {
       } else {
         myDataBase.insertOne({
           username: req.body.username,
-          password: req.body.password
+          password: hash
         }, (err, doc) => {
           if (err) {
             res.redirect('/')
