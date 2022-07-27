7e9969223aa6f22ce8223d2a83c84ddc5fa22000
jade@itnt.co.za <jade@itnt.co.za>
2022-07-27 13:28:23 +0200

    registering a new user, authenticating them and redirecting to the profile page. Also fixed the logout route

--- server.js
+++ server.js
@@ -43,15 +43,17 @@ myDB(async client => {
   ))
 
   app.route('/').get((req, res) => {
-    res.render('pug', { title: 'Connected to Database', message: 'Please login', showLogin: true });
+    res.render('pug', { title: 'Connected to Database', message: 'Please login', showLogin: true, showRegistration: true });
   });
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render('/profile', { username: req.user.username })
+    res.render(process.cwd() + '/views/pug/profile', { username: req.user.username })
   })
 
-  app.route('/logout').get(ensureAuthenticated, (req, res) => {
-    req.logout()
+  app.route('/logout').get((req, res) => {
+    req.logout((err) => {
+      if (err) { return next(err) }
+    })
     res.redirect('/')
   })
 
@@ -59,6 +61,29 @@ myDB(async client => {
     passport.authenticate('local', { failureRedirect: '/' }),
     (req, res) => {
       res.redirect('/profile')
+  })
+
+  app.post('/register', (req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) {
+        next(err)
+      } else if (user) {
+        res.redirect('/')
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        }, (err, doc) => {
+          if (err) {
+            res.redirect('/')
+          } else {
+            next(null, doc.ops[0])
+          }
+        })
+      }
+    })
+  }, passport.authenticate('local', { failureRedirect: '/' }), (req, res, next) => {
+    res.redirect('/profile')
   })
 
   app.use((req, res, next) => {
