11de53a91a149c542ad00960ca2d8d8e98d09004
jade@itnt.co.za <jade@itnt.co.za>
2022-07-28 11:05:36 +0200

    reverting further changes

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ const app = express();
 const passport = require('passport');
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-const passportSocketIo = require('passport.socketio')
-const cookieParser = require('cookie-parser')
-const MongoStore = require('connect-mongodb-session')(session)
-const URI = process.env.MONGO_URI
-const store = new MongoStore({ url: URI })
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -23,9 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
-  RESAVE: true,
-  key: 'express.sid',
-  store: store,
+  resave: true,
   saveUninitialized: true,
   cookie: { secure: false }
 }))
@@ -44,24 +37,15 @@ myDB(async client => {
 
   let currentUsers = 0
 
-  io.use(passportSocketIo.authorize({
-    cookieParser: cookieParser,
-    key: 'express.sid',
-    secret: process.env.SESSION_SECRET,
-    store: store,
-    success: onAuthorizeSuccess,
-    fail: onAuthorizeFail
-  }))
-
   io.on('connection', socket => {
     ++currentUsers
     io.emit('user count', currentUsers)
-    console.log(`user ${socket.request.user.username} has connected`)
+    console.log(`user has connected`)
 
     socket.on('disconnect', () => {
       --currentUsers
       io.emit('user count', currentUsers)
-      console.log(`user ${socket.request.user.username} has disconnected`)
+      console.log(`user has disconnected`)
     })
   })
 
@@ -70,17 +54,6 @@ myDB(async client => {
     res.render('pug', { title: e, message: 'Unable to login'})
   })
 })
-
-function onAuthorizeSuccess(data, accept) {
-  console.log('Successful connection to socket.io')
-  accept(null, true)
-}
-
-function onAuthorizeFail(data, message, error, accept) {
-  if (error) throw new Error(message)
-  console.log('failed connection to socket.io')
-  accept(null, false)
-}
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
