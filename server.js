523150042dcc0ee9934f40f483b291be8155fa22
jade@itnt.co.za <jade@itnt.co.za>
2022-07-28 11:55:33 +0200

    authorization with socket.io

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const app = express();
 const passport = require('passport');
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+const passportSocketIo = require('passport.socketio')
+const cookieParser = require('cookie-parser')
+const MongoStore = require('connect-mongo')(session)
+const URI = process.env.MONGO_URI
+const store = new MongoStore({url:URI})
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -20,7 +25,9 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
 }))
 
 app.use(passport.initialize())
@@ -34,11 +41,22 @@ myDB(async client => {
   routes(app, myDataBase)
   auth(app, myDataBase)
 
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: 'express.sid',
+      secret: process.env.SESSION_SECRET,
+      store: store,
+      success: onAuthorizeSuccess,
+      fail: onAuthorizeFail
+    })
+  )
+
   let currentUsers = 0
   io.on('connection', socket => {
     ++currentUsers
     io.emit('user count', currentUsers)
-    console.log(`user connected`)
+    console.log(`user ${socket.request.user.username} connected`)
 
     socket.on('disconnect', () => {
       --currentUsers
@@ -52,6 +70,20 @@ myDB(async client => {
     res.render('pug', { title: e, message: 'Unable to login'})
   })
 })
+
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
